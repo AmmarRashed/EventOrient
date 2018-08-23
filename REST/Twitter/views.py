8c2d4c8e8fa703228acc7630bc7128e5f9abe7fb
Ammar Rashed <ammarrashed@std.sehir.edu.tr>
2018-08-23 14:15:26 +0300

    Friendship network Checkbox now dynamically connected to D3

--- REST/Twitter/views.py
+++ REST/Twitter/views.py
@@ -109,17 +109,35 @@ def label_nodes_SCCs(G):
     return G
 
 
-def label_nodes_communities(G, recalculate=False):
-    if recalculate:
+def calculate_communities(G):
+    g = networkx_to_snappy(G)
+    CmtyV = snap.TCnComV()
+    modularity = snap.CommunityGirvanNewman(g, CmtyV)
+    nodes_communities = {}  # {node: [community]}
+    for i, Cmty in enumerate(CmtyV):
+        for NI in Cmty:
+            nodes_communities.setdefault(NI, [])
+            nodes_communities[NI].append(i + 2)
+    return nodes_communities
 
-        g = networkx_to_snappy(G)
-        CmtyV = snap.TCnComV()
-        modularity = snap.CommunityGirvanNewman(g, CmtyV)
-        nodes_communities = {}  # {node: [community]}
-        for i, Cmty in enumerate(CmtyV):
-            for NI in Cmty:
-                nodes_communities.setdefault(NI, [])
-                nodes_communities[NI].append(i + 2)
+
+def label_nodes_communities(G, save=False):
+    nodes_communities = None
+    try:
+        nodes_communities = json.load(open(root_dir + "/static/{0}_{1}_{2}.json".format(
+            dates[date_index], bidir, foci_checked),
+                                           'r'))
+    except IOError:
+        save = not sum([degree_threshold, btw_threshold, pagerank_threshold,
+                        closeness_threshold, eigenvector_threshold, clust_threshold])  # The entire network at that date
+
+
+    if recalculate_coms_checked:
+        print("CALCULATING COMMUNITIES", len(G.nodes()))
+        nodes_communities = calculate_communities(G)
+        if save:
+            json.dump(nodes_communities, open(root_dir + "/static/{0}_{1}_{2}.json".format(
+                dates[date_index], bidir, foci_checked), 'w'))
 
     for node in G.nodes():
         try:
@@ -127,12 +145,12 @@ def label_nodes_communities(G, recalculate=False):
             m = str(user['community'])
         except KeyError:
             m = "0"
-        if recalculate and 'foci' not in m:
+        if recalculate_coms_checked and 'foci' not in m:
             m = str(nodes_communities[node][0])
         G.nodes[node]["community"] = m
     return G
 
-def construct_network(connections, rec_metrics=True, include_foci=True, recalculate_coms=False, recalculate_SCCs=True):
+def construct_network(connections):
     G = nx.DiGraph()
     truncate = lambda x: int(str(int(x))[:9])
     for _, row in connections.iterrows():
@@ -140,30 +158,33 @@ def construct_network(connections, rec_metrics=True, include_foci=True, recalcul
         t = row["to_user_id"]
         from_ = truncate(f)
         to = truncate(t)
-        if not include_foci and (from_ in orgs or to in orgs):
+        if not foci_checked and (from_ in orgs or to in orgs):
             continue
         if from_ in twitter_users.truncated_id and to in twitter_users.truncated_id:
             G.add_edge(from_, to)
+
     new_G, bidir_edges = get_bidir_edges(G)
 
+    # filtered_g = filter_by(recalculate_metrics(new_G))
+    filtered_g = filter_by(recalculate_metrics(new_G))
 
     # augs = ["name", "screen_name", "match_name", "followers_count", "friends_count", "lang"]
     # getting pre-calculated communities >> THIS IS JUST OPTIONAL
     augs = ["name", "screen_name", "match_name", "followers_count", "friends_count", "lang"]
 
-    G = label_nodes_communities(G, recalculate = recalculate_coms)
+    filtered_g = label_nodes_communities(filtered_g)
 
-    if recalculate_SCCs:
-        G = label_nodes_SCCs(G)
+    if recalculate_SCCs_checked:
+        filtered_g = label_nodes_SCCs(filtered_g)
 
 
 
-    for node in G.nodes():
+    for node in filtered_g.nodes():
         user = twitter_users.loc[node]
         for aug in augs:
-            G.nodes[node][aug] = user[aug]
+            filtered_g.nodes[node][aug] = user[aug]
 
-    return recalculate_metrics(G, parse=False, centralities=rec_metrics), bidir_edges
+    return filtered_g, bidir_edges
 
 
 
@@ -236,7 +257,8 @@ def get_avg_metric(graph, metric):
 
 
 @lru_cache(maxsize=None)
-def recalculate_metrics(nxg, parse=True, centralities=True):
+def recalculate_metrics(nxg, centralities=True):
+
     nxg = nxg.to_directed()
     for ix, deg in nxg.degree(nxg.nodes()):
         nxg.node[ix]['degree'] = deg
@@ -262,15 +284,14 @@ def recalculate_metrics(nxg, parse=True, centralities=True):
             for metric_name, metric in cntr_metrics.items():
                 for ix, v in metric.items():
                     nxg.node[ix][metric_name] = v
-        except:
+        except Exception as e:
+            print("ERROR", e)
             pass
 
 
-    if parse:
-        data = json_graph.node_link_data(nxg)
-        with open(root_dir + "/static/networks/latest_tw_ntw.json", 'w') as f:
-            json.dump(data, f, indent=4)
-        return data
+    # data = json_graph.node_link_data(nxg)
+    # with open(root_dir + "/static/networks/latest_tw_ntw.json", 'w') as f:
+    #     json.dump(data, f, indent=4)
     return nxg
 
 
@@ -316,7 +337,7 @@ def twitter_connections(request):
         recalculate_SCCs_checked = check[request.POST.get("recalculate_SCCs_checked", "on")]
 
         foci_checked = check[request.POST.get("toggleFociCB", False)]
-        bidir = check[request.POST.get("bidir", False)]
+        bidir = check[request.POST.get("toggleBidir", False)]
 
         do_filter = foci_checked != DEFAULT_FOCI_CHECKED or bidir != DEFAULT_BIDIR
         if not do_filter:
@@ -331,23 +352,10 @@ def twitter_connections(request):
             #     data = deepcopy(twitter_connections_json)
             # else:
             cons = get_connections_by_date(dates[date_index])
-            nw, bidir_edges = construct_network(cons, recalculate_checked, foci_checked,
-                                                recalculate_coms_checked, recalculate_SCCs_checked)
-            data = nx.node_link_data(nw)
-            with open('temp2.json', 'w') as f:
-                print(len(data["nodes"]), "SIZE")
-                json.dump(data, f, indent=4)
-            g = json_graph.node_link_graph(data, directed=True)
+            nw, bidir_edges = construct_network(cons)
 
-            filtered_twitter_connections = filter_by(g,
-                                                     degree_threshold,
-                                                     btw_threshold,
-                                                     pagerank_threshold,
-                                                     closeness_threshold,
-                                                     eigenvector_threshold,
-                                                     clust_threshold,
-                                                     recalculate_checked,
-                                                     directed=True)
+            filtered_twitter_connections = nx.node_link_data(nw)
+
         else:
             filtered_twitter_connections = deepcopy(twitter_connections_json)
 
@@ -365,12 +373,13 @@ def twitter_connections(request):
         avgs = {"avg_"+metric:get_avg_metric(filtered_twitter_connections, metric) for metric in size_metrics}
         sizes = [n[size_metric] for n in filtered_twitter_connections["nodes"]]
 
-    context = {"degree_threshold": degree_threshold,
-               "btw_threshold": btw_threshold,
-               "pagerank_threshold": pagerank_threshold,
-               "closeness_threshold": closeness_threshold,
-               "eigenvector_threshold": eigenvector_threshold,
-               "size_metrics": size_metrics,
+    # "degree_threshold": degree_threshold,
+    # "btw_threshold": btw_threshold,
+    # "pagerank_threshold": pagerank_threshold,
+    # "closeness_threshold": closeness_threshold,
+    # "eigenvector_threshold": eigenvector_threshold,
+
+    context = {"size_metrics": size_metrics,
                "recalculate_checked":int(recalculate_checked),
                "recalculate_coms_checked": int(recalculate_coms_checked),
                "nodes_number": len(filtered_twitter_connections["nodes"]),
@@ -397,26 +406,23 @@ def twitter_connections(request):
 
 
 @lru_cache(maxsize=None)
-def filter_by(g, degree, btw, pagerank, closeness, eigenv, clust_threshold,recalculate_node_metrics, directed=True):
-    metrics = {"degree":degree, "betweenness":btw, "pagerank":pagerank,
-               "closeness_centrality":closeness, "eigenvector_centrality":eigenv,
+def filter_by(g):
+
+    metrics = {"degree":degree_threshold, "betweenness":btw_threshold, "pagerank":pagerank_threshold,
+               "closeness_centrality":closeness_threshold, "eigenvector_centrality":eigenvector_threshold,
                "clustering_coefficient":clust_threshold}
     c = g.copy()
     for node in g.nodes():
         invalid = False
-        for metric in metrics:
-            threshold = metrics[metric]
-            try:
-                if g.nodes[node][metric] < threshold:
-                    invalid = True
-                    break
-            except KeyError:
-                continue
+        for metric, threshold in metrics.iteritems():
+            if g.nodes[node][metric] < threshold:
+                invalid = True
+                break
         if invalid:
             c.remove_node(node)
-    if recalculate_node_metrics and len(c.nodes) != len(twitter_connections_json["nodes"]):
+    if recalculate_checked:
         return recalculate_metrics(c)
-    return json_graph.node_link_data(c)
+    return c
 
 
 def load_twitter_connections_json(request):
